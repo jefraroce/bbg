071faac6583dcf2920416f2e72a45ae7009d790c
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2020-11-19 20:01:55 -0500

    Implementación y prueba de sweetalert.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import swal from 'sweetalert';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor() {
+    // swal('Hola Jeisson') :)
+  }
+
 }
