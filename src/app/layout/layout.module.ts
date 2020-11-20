f764c08e28193d1555e66da9a02c7cf850ef99dc
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2020-11-19 22:15:48 -0500

    Creación del modulo de diseño (layout).

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+
+// Cargamos este modulo para que los enlaces dentro del componente de navegación funcionen.
+import { RouterModule } from '@angular/router';
+
+// Componentes
+import { NavegacionComponent } from './navegacion/navegacion.component';
+import { PublicidadComponent } from './publicidad/publicidad.component';
+
+@NgModule({
+  declarations: [
+    NavegacionComponent,
+    PublicidadComponent
+  ],
+  imports: [
+    RouterModule
+  ],
+  exports: [
+    NavegacionComponent,
+    PublicidadComponent
+  ]
+})
+export class LayoutModule { }
