7f2d49d41255540e5bd00d989be864cdf557f8e9
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2020-11-19 22:17:25 -0500

    Organización del AppModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { LayoutModule } from './layout/layout.module';
 
+// Componentes
 import { AppComponent } from './app.component';
 import { LibroComponent } from './componentes/libro/libro.component';
 import { CarritoComponent } from './paginas/carrito/carrito.component';
@@ -10,6 +12,7 @@ import { ComprasComponent } from './paginas/compras/compras.component';
 import { CompraPorIdComponent } from './paginas/compra-por-id/compra-por-id.component';
 import { NoEncontradaComponent } from './paginas/no-encontrada/no-encontrada.component';
 import { LibroPorIdComponent } from './paginas/libro-por-id/libro-por-id.component';
+import { PruebasComponent } from './paginas/pruebas/pruebas.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +22,14 @@ import { LibroPorIdComponent } from './paginas/libro-por-id/libro-por-id.compone
     ComprasComponent,
     CompraPorIdComponent,
     NoEncontradaComponent,
-    LibroPorIdComponent
+    LibroPorIdComponent,
+    PruebasComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
