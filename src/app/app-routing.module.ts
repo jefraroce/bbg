28da383d08278bd08cfb55e1af99695b630090fe
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2020-11-19 20:17:19 -0500

    Creada página para revisar las pruebas construidas durante el aprendizaje.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+// Carga de componentes para las páginas por defecto
 import { CarritoComponent } from './paginas/carrito/carrito.component';
 import { CompraPorIdComponent } from './paginas/compra-por-id/compra-por-id.component';
 import { ComprasComponent } from './paginas/compras/compras.component';
 import { InicioDeSesionComponent } from './paginas/inicio-de-sesion/inicio-de-sesion.component';
-
 import { InicioComponent } from './paginas/inicio/inicio.component';
 import { LibroPorIdComponent } from './paginas/libro-por-id/libro-por-id.component';
 import { NoEncontradaComponent } from './paginas/no-encontrada/no-encontrada.component';
 import { RegistroComponent } from './paginas/registro/registro.component';
+
+// En este componente tenemos los demos que probamos inicialmente
+import { PruebasComponent } from './paginas/pruebas/pruebas.component';
 
 const routes: Routes = [
   {
@@ -43,6 +47,10 @@ const routes: Routes = [
   {
     path: 'compras/:id',
     component: CompraPorIdComponent
+  },
+  {
+    path: 'pruebas',
+    component: PruebasComponent
   },
   {
     path: '**',
